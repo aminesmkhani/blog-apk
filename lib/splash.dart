ce663179f9913e7871e3b1d357f7fb3ecd1828c8
Aмιη <amin.esmkhani75@gmail.com>
2024-08-12 07:17:28 -0800

    add redirect splash page to home page!

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,8 +1,24 @@
 import 'package:blogclub/gen/assets.gen.dart';
+import 'package:blogclub/home.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2)).then((value) {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
+        return const HomeScreen();
+      }));
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +26,11 @@ class SplashScreen extends StatelessWidget {
       body: Stack(children: [
         Positioned.fill(
           child: Assets.img.background.splash.image(fit: BoxFit.cover),
-          ),
-          Center(child: Assets.img.icons.logo.svg(),
-      ],),
+        ),
+        Center(
+          child: Assets.img.icons.logo.svg(),
+        )
+      ]),
     );
   }
 }
