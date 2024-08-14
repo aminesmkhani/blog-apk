6e30b06939aaf30c656ffc27260aee5d00063cdc
Aмιη <amin.esmkhani75@gmail.com>
2024-08-14 04:26:26 -0800

    depart 2 section logo and content !

--- lib/auth.dart
+++ lib/auth.dart
@@ -7,17 +7,36 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData themeData = Theme.of(context);
+    final TabTextStyle = TextStyle(
+      
+    );
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Assets.img.icons.logo.svg(),
-            Container(
-              decoration: BoxDecoration(
-                color: themeData.
-                colorScheme.primary,
-                
+            Padding(
+              padding: const EdgeInsets.only(top: 40, bottom: 30),
+              child: Assets.img.icons.logo.svg(width: 110),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(32),
+                      topRight: Radius.circular(32)),
+                  color: themeData.colorScheme.primary,
                 ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('Login'.toUpperCase()),
+                        Text('Register'.toUpperCase()),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
