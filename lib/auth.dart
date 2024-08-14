bb9b3b1efbd0923369c5b77a9961cca23dd82ccd
Aмιη <amin.esmkhani75@gmail.com>
2024-08-14 05:39:39 -0800

    add all elements styel!

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:blogclub/gen/assets.gen.dart';
+import 'package:blogclub/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatelessWidget {
@@ -46,61 +49,110 @@ class AuthScreen extends StatelessWidget {
                       ),
                     ),
                     Expanded(
-                      child: SingleChildScrollView(
-                        child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(32),
-                                  topRight: Radius.circular(32)),
-                              color: themeData.colorScheme.surface),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(32),
+                                topRight: Radius.circular(32)),
+                            color: themeData.colorScheme.surface),
+                        child: SingleChildScrollView(
                           child: Padding(
                             padding: const EdgeInsets.all(32),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                 Text(
-                                  'Welcome Back',
-                                  style: themeData.textTheme.headlineMedium
-                                  ),
-                                  const SizedBox(height: 12,),
-                                 Text('Sign in with your account',style: themeData.textTheme.titleMedium!.apply(fontSizeFactor: 0.8),),
-                                  SizedBox(height: 16,),
-                                 TextField(
+                                Text('Welcome Back',
+                                    style: themeData.textTheme.headlineMedium),
+                                const SizedBox(
+                                  height: 12,
+                                ),
+                                Text(
+                                  'Sign in with your account',
+                                  style: themeData.textTheme.titleMedium!
+                                      .apply(fontSizeFactor: 0.8),
+                                ),
+                                SizedBox(
+                                  height: 16,
+                                ),
+                                TextField(
                                   decoration:
                                       InputDecoration(label: Text('UserName')),
                                 ),
-                                 TextField(
+                                TextField(
                                   obscureText: true,
                                   enableSuggestions: false,
                                   autocorrect: false,
-                                  decoration:
-                                      InputDecoration(label: Text('Password')),
+                                  decoration: InputDecoration(
+                                    label: Text('Password'),
+                                    suffixIcon: TextButton(
+                                      style: ButtonStyle(
+                                        tapTargetSize: MaterialTapTargetSize.shrinkWrap
+                                      ),
+                                      onPressed: () {},
+                                      child: Text('Show'),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 24,
                                 ),
                                 ElevatedButton(
+                                    style: ButtonStyle(
+                                      minimumSize: WidgetStateProperty.all(
+                                        Size(MediaQuery.of(context).size.width,
+                                            60),
+                                      ),
+                                      shape: WidgetStateProperty.all(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(12)),
+                                      ),
+                                      backgroundColor: WidgetStateProperty.all(
+                                          themeData.primaryColor),
+                                    ),
                                     onPressed: () {},
-                                    child: Text('Login'.toUpperCase())),
+                                    child: Text(
+                                      'Login'.toUpperCase(),
+                                      style: TextStyle(
+                                        fontFamily: FontFamily.avenir,
+                                        color: Colors.white,
+                                      ),
+                                    )),
                                 Row(
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                    const Text('Forgot your password?'),
+                                    const Text('Forgot your password?',),
+                                    SizedBox(width: 8,),
                                     TextButton(
                                         onPressed: () {},
                                         child: const Text('Reset Here')),
                                   ],
                                 ),
+                                SizedBox(height: 20,),
                                 Center(
-                                  child: Text('OR SIGN IN WITH',),
+                                  child: Text(
+                                    'OR SIGN IN WITH',
+                                    style: TextStyle(letterSpacing: 2)
+                                  ),
                                 ),
+                                SizedBox(height: 20,),
                                 Row(
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                  Assets.img.icons.google.image(width: 36,height: 36),
-                                  SizedBox(width: 30,),
-                                  Assets.img.icons.facebook.image(width: 36,height: 36),
-                                  SizedBox(width: 30,),
-                                  Assets.img.icons.twitter.image(width: 36,height: 36),
-                                
-                                ],),
+                                    Assets.img.icons.google
+                                        .image(width: 36, height: 36),
+                                    SizedBox(
+                                      width: 30,
+                                    ),
+                                    Assets.img.icons.facebook
+                                        .image(width: 36, height: 36),
+                                    SizedBox(
+                                      width: 30,
+                                    ),
+                                    Assets.img.icons.twitter
+                                        .image(width: 36, height: 36),
+                                  ],
+                                ),
                               ],
                             ),
                           ),
