162891eb1842834c183ed61f1babfe3e3ab144ef
Aмιη <amin.esmkhani75@gmail.com>
2024-08-14 13:37:28 -0800

    Complete Article Page!

--- lib/article.dart
+++ lib/article.dart
@@ -43,95 +43,111 @@ class ArticleScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            floating: true,
-            title: const Text('Article'),
-            actions: [
-              IconButton(
-                  onPressed: () {}, icon: Icon(Icons.more_horiz_rounded)),
-              const SizedBox(
-                width: 16,
-              )
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                title: const Text('Article'),
+                actions: [
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.more_horiz_rounded)),
+                  const SizedBox(
+                    width: 16,
+                  )
+                ],
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate.fixed([
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    child: Text(
+                      'Four Things Every Woman Needs To Know',
+                      style: themeData.textTheme.headlineMedium,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(right: 16, left: 32, bottom: 32),
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Assets.img.stories.story9
+                              .image(width: 48, height: 48, fit: BoxFit.cover),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Richard Geravin',
+                                  style: themeData.textTheme.bodyLarge!
+                                      .copyWith(fontWeight: FontWeight.w400)),
+                              const SizedBox(
+                                height: 4,
+                              ),
+                              const Text('2m ago'),
+                            ],
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            showSnackBar(context, 'Share SocialNetworks');
+                          },
+                          icon: const Icon(CupertinoIcons.share),
+                          color: themeData.colorScheme.primary,
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            showSnackBar(context, 'BookMark Saved');
+                          },
+                          icon: const Icon(CupertinoIcons.bookmark),
+                          color: themeData.colorScheme.primary,
+                        ),
+                      ],
+                    ),
+                  ),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(32),
+                        topRight: Radius.circular(32)),
+                    child: Assets.img.background.singlePost.image(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(32, 32, 32, 16),
+                    child: Text(
+                      'A man’s sexuality is never your mind responsibility.',
+                      style: themeData.textTheme.headlineSmall,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(32, 0, 32, 32),
+                    child: Text(
+                      'This one got an incredible amount of backlash the last time I said it, so I’m going to say it again: a man’s sexuality is never, ever your responsibility, under any circumstances. Whether it’s the fifth date or your twentieth year of marriage, the correct determining factor for whether or not you have sex with your partner isn’t whether you ought to “take care of him” or “put out” because it’s been a while or he’s really horny — the correct determining factor for whether or not you have sex is whether or not you want to have sex.This one got an incredible amount of backlash the last time I said it, so I’m going to say it again: a man’s sexuality is never, ever your responsibility, under any circumstances. Whether it’s the fifth date or your twentieth year of marriage, the correct determining factor for whether or not you have sex with your partner isn’t whether you ought to “take care of him” or “put out” because it’s been a while or he’s really horny — the correct determining factor for whether or not you have sex is whether or not you want to have sex.This one got an incredible amount of backlash the last time I said it, so I’m going to say it again: a man’s sexuality is never, ever your responsibility, under any circumstances. Whether it’s the fifth date or your twentieth year of marriage, the correct determining factor for whether or not you have sex with your partner isn’t whether you ought to “take care of him” or “put out” because it’s been a while or he’s really horny — the correct determining factor for whether or not you have sex is whether or not you want to have sex.',
+                      style: themeData.textTheme.bodyLarge,
+                    ),
+                  ),
+                ]),
+              ),
             ],
           ),
-          SliverList(
-            delegate: SliverChildListDelegate.fixed([
-              Padding(
-                padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-                child: Text(
-                  'Four Things Every Woman Needs To Know',
-                  style: themeData.textTheme.headlineMedium,
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.only(right: 16, left: 32, bottom: 32),
-                child: Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Assets.img.stories.story9
-                          .image(width: 48, height: 48, fit: BoxFit.cover),
-                    ),
-                    const SizedBox(
-                      width: 16,
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Richard Geravin',
-                              style: themeData.textTheme.bodyLarge!
-                                  .copyWith(fontWeight: FontWeight.w400)),
-                          const SizedBox(
-                            height: 4,
-                          ),
-                          const Text('2m ago'),
-                        ],
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        showSnackBar(context, 'Share SocialNetworks');
-                      },
-                      icon: const Icon(CupertinoIcons.share),
-                      color: themeData.colorScheme.primary,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        showSnackBar(context, 'BookMark Saved');
-                      },
-                      icon: const Icon(CupertinoIcons.bookmark),
-                      color: themeData.colorScheme.primary,
-                    ),
-                  ],
-                ),
-              ),
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(32),
-                    topRight: Radius.circular(32)),
-                child: Assets.img.background.singlePost.image(),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(32, 32, 32, 16),
-                child: Text(
-                  'A man’s sexuality is never your mind responsibility.',
-                  style: themeData.textTheme.headlineSmall,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(32, 0, 32, 32),
-                child: Text(
-                  'This one got an incredible amount of backlash the last time I said it, so I’m going to say it again: a man’s sexuality is never, ever your responsibility, under any circumstances. Whether it’s the fifth date or your twentieth year of marriage, the correct determining factor for whether or not you have sex with your partner isn’t whether you ought to “take care of him” or “put out” because it’s been a while or he’s really horny — the correct determining factor for whether or not you have sex is whether or not you want to have sex.This one got an incredible amount of backlash the last time I said it, so I’m going to say it again: a man’s sexuality is never, ever your responsibility, under any circumstances. Whether it’s the fifth date or your twentieth year of marriage, the correct determining factor for whether or not you have sex with your partner isn’t whether you ought to “take care of him” or “put out” because it’s been a while or he’s really horny — the correct determining factor for whether or not you have sex is whether or not you want to have sex.This one got an incredible amount of backlash the last time I said it, so I’m going to say it again: a man’s sexuality is never, ever your responsibility, under any circumstances. Whether it’s the fifth date or your twentieth year of marriage, the correct determining factor for whether or not you have sex with your partner isn’t whether you ought to “take care of him” or “put out” because it’s been a while or he’s really horny — the correct determining factor for whether or not you have sex is whether or not you want to have sex.',
-                  style: themeData.textTheme.bodyLarge,
-                ),
-              ),
-            ]),
-           
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 116,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [
+                    themeData.colorScheme.surface,
+                    themeData.colorScheme.surface.withOpacity(0)
+                  ])),
+            ),
           ),
         ],
       ),
