51847c7d861318d13f1815b904bb20f4f8f29013
Aмιη <amin.esmkhani75@gmail.com>
2024-08-16 23:05:16 -0800

    create profile image and texts!

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,25 +1,61 @@
+import 'package:blogclub/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class ProfileScreen extends StatelessWidget{
+class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    appBar: AppBar(
-      elevation: 0,
-      title: Text('Profile'),
-      actions: [
-        Icon(Icons.more_horiz_rounded),
-        SizedBox(width: 16,)
-      ],
-    ),
-    body: Column(
-      children: [
+    final themeData = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
         
-      ],
-    ),
-   );
+        title: Text('Profile'),
+        actions: [
+          IconButton(icon: Icon(Icons.more_horiz_rounded), onPressed: () { 
+
+          },),
+          SizedBox(
+            width: 16,
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(32, 0, 32, 32),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: themeData.colorScheme.surface,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 20,
+                  color: themeData.colorScheme.onSurface.withOpacity(0.2),
+                  ),
+              ]
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Assets.img.stories.story8.image(width: 84,height: 84),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text('@jovieden'),
+                          Text('Jovi Daniel'),
+                          Text('UX Designer'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
